51b15ad513d15fccc98f7f61431ce45644b871d9
test <422538722@qq.com>
2017-03-27 22:33:31 +0800

    2.1.1

---
+++ 2.1.1/BOOL类型/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BOOL类型
+//
+//  Created by 李维佳 on 2017/3/22.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        BOOL b1 = YES;
+        BOOL b2 = NO;
+        NSLog(@"b1:%d,b2:%d",b1,b2);
+    }
+    return 0;
+}
---
+++ 2.1.1/float类型/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  float类型
+//
+//  Created by 李维佳 on 2017/3/22.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        float f = 10.1234;
+        NSLog(@"%f,%.2f", f,f);
+    }
+    return 0;
+}
